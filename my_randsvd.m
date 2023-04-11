d20f19ada208e17621558cd37559be996ce240a4
zhengbo53 <1219108879@outlook.com>
2023-04-11 19:45:44 +0100

    add more mode to my_randsvd

--- my_randsvd.m
+++ my_randsvd.m
@@ -1,54 +1,80 @@
-function [A,evals] = my_randsvd(n, kappa, mode)
+function [A,svals] = my_randsvd(n, kappa, mode)
 %MY_RANDSVD Customized gallery('randsvd')
-% Generate symmetric matrices with predefined condition number
-% and different distribution of singular values.
+% [A, SVALS] = my_randsvd(N, KAPPA, MODE) is a random symmetric
+% matrix of order N with cond(A) = KAPPA and singular values
+% from distribution MODE.
+%
+% SVALS contains the singular values of A. 
+%
 % Input :
-%      n : dimension of the output
-%  kappa : pre-defined 2-norm condition number for the output,
-%          if kappa is negative, it will return symmetric p.d. matrix.
-%		   Default value is 100. 
-%   mode : "geo" or "ari", representing geometrically distributed
-%          and arithmetically distributed singular values respectively.  
-%          Default value is 'geo'.
+%      N : Size of the matrix.
+%  KAPPA : pre-defined 2-norm condition number for the output,
+%          if kappa is negative, it will return a symmetric
+%          positive definite matrix.
+%		   Default value is 100.
+%   MODE : one of the following values:
+%		1: one large singular value,
+%		2: one small singular value,
+%		3: geometrically distributed singular values,
+%		4: arithmetically distributed singular values,
+%		5: random singular values with uniformly distributed
+%		logarithm. 
+
+% Reference:
+% The MATLAB 'gallery' function.
+% https://uk.mathworks.com/help/matlab/ref/gallery.html 
 
 classname = 'double';
+
 switch nargin
 	case 1
 		kappa = 100;
-		mode = 'Geo';
+		mode = 3;
 	case 2
-		mode = 'Geo';
+		mode = 3;
 end
 
-if isempty(mode), mode = 'Geo'; end
+% check if we need export p.d. matrix
 if sign(kappa) == 1, pd = false; else, pd = true; end
 kappa = abs(kappa);
 
-switch mode % Set up vector sigma of singular values.
-  case {'Geometric','geometric','Geo','geo'} 
-    % geometrically distributed singular values
-    factor = kappa^(-1/(n-1));
-    sigma = factor.^(0:n-1);
-  case {'Arithematic', 'arithematic', 'Ari', 'ari'}
-    % arithmetically distributed singular values
-    sigma = ones(n,1) - (0:n-1)'/(n-1)*(1-1/kappa); sigma = sigma';
-  otherwise
-    error(message('MATLAB:randsvd:invalidMode'));
+switch mode % Set up vector of singular values
+	case 1
+		sigma = ones(n,1)./kappa;
+		sigma(1) = 1;
+
+	case 2
+		sigma = ones(n,1);
+		sigma(n) = 1/kappa;
+
+	case 3
+		factor = kappa^(-1/(n-1));
+		sigma = factor.^(0:n-1);
+
+	case 4
+		sigma = ones(n,1) - (0:n-1)'/(n-1)*(1-1/kappa);
+
+	case 5    % In this case cond(A) <= kappa.
+		sigma = exp( -rand(n,1)*log(kappa) );
+
+	otherwise
+		error(message('MATLAB:randsvd:invalidMode'));
 end
 
 sigma = cast(sigma,classname);
 
 if pd
-    signs = sign(rand(1,n-2));
-    sigma(2:n-1) = sigma(2:n-1).* signs;
+	signs = sign(rand(1,n-2));
+	sigma(2:n-1) = sigma(2:n-1).* signs;
 end
+
 Q = qmult(n,1,classname);
-A = Q*diag(sigma)*Q'; 
+A = Q*diag(sigma)*Q';
 A = (A + A')/2; % ensure symmetry
 
 % output true eigenvalues/singular values
 if nargout == 2
-	evals = sigma;
+	svals = sigma;
 end
 
 end
@@ -77,41 +103,41 @@ function B = qmult(A,method,classname)
 %   Copyright 1984-2020 The MathWorks, Inc.
 
 if nargin < 2 || isempty(method)
-    method = 0;
+	method = 0;
 end
 %  Handle scalar A.
 if isscalar(A)
-   n = A;
-   A = eye(n,classname);
+	n = A;
+	A = eye(n,classname);
 else
-    n = size(A, 1);
+	n = size(A, 1);
 end
 if isempty(A) % nothing to do.
-    B = A;
-    return
+	B = A;
+	return
 end
 if method == 1
-   [Q,R] = qr(randn(n));
-   B = Q*diag(sign(diag(R)))*A;
-   return
+	[Q,R] = qr(randn(n));
+	B = Q*diag(sign(diag(R)))*A;
+	return
 end
 d = zeros(n,1,classname);
 for k = n-1:-1:1
-    % Generate random Householder transformation.
-    x = randn(n-k+1,1);
-    s = norm(x);
-    sgn = mysign(x(1));
-    s = sgn*s;
-    d(k) = -sgn;
-    x(1) = x(1) + s;
-    beta = s*x(1);
-    % Apply the transformation to A.
-    y = x'*A(k:n,:);
-    A(k:n,:) = A(k:n,:) - x*(y/beta);
+	% Generate random Householder transformation.
+	x = randn(n-k+1,1);
+	s = norm(x);
+	sgn = mysign(x(1));
+	s = sgn*s;
+	d(k) = -sgn;
+	x(1) = x(1) + s;
+	beta = s*x(1);
+	% Apply the transformation to A.
+	y = x'*A(k:n,:);
+	A(k:n,:) = A(k:n,:) - x*(y/beta);
 end
 % Tidy up signs.
 for i=1:n-1
-    A(i,:) = d(i)*A(i,:);
+	A(i,:) = d(i)*A(i,:);
 end
 A(n,:) = A(n,:)*mysign(randn);
 B = A;
